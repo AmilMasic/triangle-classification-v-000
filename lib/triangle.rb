27b68acf57ba88a4f3f8baa3e05160dc7dfca332
Amil Masic <amil.masic@gmail.com>
2018-10-07 15:12:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,11 +3,11 @@ class Triangle
 
   attr_accessor :a, :b, :c
 
-  # def initialize
-  #   @a = a
-  #   @b = b
-  #   @c = c
-  # end
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
 
   def kind
     if @a == @b && @a == @c && @b == @c
